94a2814d3e4150d4a4cc4f37ad3a3febdfd193c9
Luis Enrique Cabello Blanco <luisecab@ucm.es>
2023-03-09 22:59:59 +0100

    Input y movement avances

--- peladontJam/Assets/Scripts/Movement & Camera/InputManager.cs
+++ peladontJam/Assets/Scripts/Movement & Camera/InputManager.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private PlayerControlls playerControlls;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        playerControlls = new PlayerControlls();
+    }
+    private void OnEnable()
+    {
+        playerControlls.Enable();
+    }
+    private void OnDisable()
+    {
+        playerControlls.Disable();
+    }
+    public Vector2 GetPlayerMovement()
+    {
+        return playerControlls.Player.Movement.ReadValue<Vector2>();
+    }
+    public Vector2 GetMouseDelta()
+    {
+        return playerControlls.Player.Look.ReadValue<Vector2>();
     }
 }
--- peladontJam/Assets/Scripts/Movement & Camera/MovementController.cs
+++ peladontJam/Assets/Scripts/Movement & Camera/MovementController.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    [SerializeField]
+    private Vector3 directionVector;
+    private float playerSpeed = 2.0f;
+    private float gravityValue = -9.81f;
+
+    private void Start()
     {
-        
+        controller = gameObject.GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+        //Vector3 move = new Vector3(direction.x, 0, direction.x);
+        controller.Move(directionVector * Time.deltaTime * playerSpeed);
+
+        if (directionVector != Vector3.zero)
+        {
+            gameObject.transform.forward = directionVector;
+        }
+        //playerVelocity.y += gravityValue * Time.deltaTime;
+        //controller.Move(playerVelocity * Time.deltaTime);
+    }
+    void SetDirection(Vector2 direction)
+    {
+        directionVector = direction;
     }
 }
