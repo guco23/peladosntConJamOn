c1c7a9d32d499e12990b040db25c5f53edabe0a1
Vivu24 <93614868+Vivu24@users.noreply.github.com>
2023-03-09 20:10:37 +0100

    Comienzo Musiquita Fresca

---
+++ peladontJam/Assets/Scripts/Sounds/MusicComponent.cs
@@ -0,0 +1,120 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicComponent : MonoBehaviour
+{
+    #region Accessors
+    #endregion
+
+    #region Paramenters
+    [SerializeField]
+    private AudioSource _audioSourceBlue;
+    [SerializeField]
+    private AudioSource _audioSourceGreen;
+    [SerializeField]
+    private AudioSource _audioSourceRed;
+    /// <summary>
+    /// tiempo de la transicion
+    /// </summary>
+    [SerializeField]
+    private float _fadeTime;
+    /// <summary>
+    /// umbral para que la transicion se complete rapidamente 
+    /// </summary>
+    [SerializeField]
+    private float _umbralVolume;
+    #endregion
+
+    #region Properties
+    [SerializeField] private bool _onBlue;
+    [SerializeField] private bool _onGreen;
+    [SerializeField] private bool _onRed;
+    #endregion
+
+    #region Methods
+    /// <summary>
+    /// cambia la pista de audio a la indicada, progresivamente bajando las otras pistas
+    /// </summary>
+    /// 
+
+    /// <summary>
+    /// cambia la pista de audio a la indicada, progresivamente bajando las otras pistas
+    /// </summary>
+    public void ChangeMusic(AudioSource currentSource)
+    {
+        if (currentSource.volume <= _umbralVolume)
+        {
+            if (!_onBlue)
+            {
+                _audioSourceBlue.volume = Mathf.Lerp(_audioSourceBlue.volume, 0, Time.deltaTime * _fadeTime);
+            }
+            if (!_onGreen)
+            {
+                _audioSourceGreen.volume = Mathf.Lerp(_audioSourceGreen.volume, 0, Time.deltaTime * _fadeTime);
+            }
+            if (!_onRed)
+            {
+                _audioSourceRed.volume = Mathf.Lerp(_audioSourceRed.volume, 0, Time.deltaTime * _fadeTime);
+            }
+
+            currentSource.volume = Mathf.Lerp(currentSource.volume, 1, Time.deltaTime * _fadeTime);
+        }
+        else
+        {
+            if (!_onBlue)
+            {
+                _audioSourceBlue.volume = 0;
+            }
+            if (!_onGreen)
+            {
+                _audioSourceGreen.volume = 0;
+            }
+            if (!_onRed)
+            {
+                _audioSourceRed.volume = 0;
+            }
+
+            currentSource.volume = 1;
+        }
+
+    }
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audioSourceBlue = transform.GetChild(0).GetComponent<AudioSource>();     // AudioSource BLUE
+        _audioSourceGreen = transform.GetChild(1).GetComponent<AudioSource>();      // AudioSource GREEN
+        _audioSourceRed = transform.GetChild(2).GetComponent<AudioSource>();      // AudioSource RED
+        ChangeMusic(_audioSourceBlue);
+    }
+
+    private void ChangeState (ref bool myState)
+    {
+        _onBlue = false;
+        _onGreen = false;
+        _onRed = false;
+        myState = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            ChangeState(ref _onBlue);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            ChangeState(ref _onGreen);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ChangeState(ref _onRed);
+        }
+
+        if (_onBlue) ChangeMusic(_audioSourceBlue);
+        if (_onGreen) ChangeMusic(_audioSourceGreen);
+        if (_onRed) ChangeMusic(_audioSourceRed);
+    }
+}
---
+++ peladontJam/Assets/Scripts/Sounds/SoundComponent.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundComponent : MonoBehaviour
+{
+    #region Accessors
+    #endregion
+
+    #region Paramenters
+    #endregion
+
+    #region Properties
+    #endregion
+
+    #region Methods
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
