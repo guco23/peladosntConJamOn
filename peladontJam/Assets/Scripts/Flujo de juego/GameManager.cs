331bc4c18fe78945a2fb77c2d5585a5069537a17
Diego Montenegro <dmonoficial22@gmail.com>
2023-03-10 22:38:47 +0100

    arreglillos

--- peladontJam/Assets/Scripts/Flujo de juego/GameManager.cs
+++ peladontJam/Assets/Scripts/Flujo de juego/GameManager.cs
@@ -6,6 +6,8 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField]
     SpawnerController[] _spawners;
+    [SerializeField]
+    GameObject _player;
 
     static private GameManager _instance;
     private UIManager _uiManager;
@@ -35,31 +37,40 @@ public class GameManager : MonoBehaviour
     public void PlayerDied()
     {
         //TODO
+
+        /*
+         * Pantalla de Game Over
+         * Reiniciar la escena.
+         */
     }
 
     public void PotionFailed()
     {
         //TODO
+        /*
+         * Obtener un efecto negativo 
+         * 
+         * 
+         */
     }
 
     public void PotionCorrect()
     {
-
+        /*
+         * Perder todos los efectos negativos.
+         * Despawnear todos los enemigos, spawnear uno nuevo de cada.
+         * 
+         */
     }
 
     /// <summary>
     /// Recorre cada spawner activando su spawn.
     /// </summary>
     private void SpawnAll() { 
-        for(int i = 0; i < _spawners.Length; i++)
-        {
-            Debug.Log(_spawners.Length);
-            _spawners[i].Spawn();
-        }
-      /*  foreach(SpawnerController spawner in _spawners)
+        foreach(SpawnerController spawner in _spawners)
         {
             spawner.Spawn();
-        } */
+        }
     }
     
     /// <summary>
