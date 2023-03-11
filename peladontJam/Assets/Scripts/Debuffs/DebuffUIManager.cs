04b84668642086738710c8fc314e2883e1b3eb6d
Adelpozo04 <addelp01@ucm.es>
2023-03-11 01:06:45 +0100

    Introduccion de los iconos

---
+++ peladontJam/Assets/Scripts/Debuffs/DebuffUIManager.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebuffUIManager : MonoBehaviour
+{
+
+    #region references
+
+    [SerializeField]
+    private GameObject[] _debuffIcons; 
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
